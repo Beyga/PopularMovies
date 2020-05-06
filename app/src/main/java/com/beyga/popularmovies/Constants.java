a94c0365be1ddbc33f5e10255de84b45618b7201
Beyga <kbejger15@gmail.com>
2020-05-06 23:38:01 +0200

    gitignore updated, BuildConfig works

--- app/src/main/java/com/beyga/popularmovies/Constants.java
+++ app/src/main/java/com/beyga/popularmovies/Constants.java
@@ -1,6 +1,5 @@
 package com.beyga.popularmovies;
 
-import com.android.volley.BuildConfig;
 
 public class Constants {
 
@@ -8,7 +7,7 @@ public class Constants {
     private static final String API_PREFIX = "?api_key=";
 
     // Type here your API key from The Movie DB
-//    private static final String API_KEY = BuildConfig.
+    private static final String API_KEY =BuildConfig.BUILD_API_KEY;
     private static final String API_KEY_CODE = API_PREFIX + API_KEY;
     static final String POPULAR_MOVIES_URL = BASE_URL + "popular" + API_KEY_CODE;
     static final String TOP_RATED_MOVIES_URL = BASE_URL + "top_rated" + API_KEY_CODE;
